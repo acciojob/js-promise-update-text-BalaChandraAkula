18a5910b77f309fcb4ed3c06bf6dcff48e64f7fb
Bala Chandra Akula <115984987+BalaChandraAkula@users.noreply.github.com>
2023-03-26 11:12:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+async function add(){
+	await delay(1000);
+	return "Hello, world!";
+}
+function delay(time) {
+  return new Promise(resolve => setTimeout(resolve, time));
+}
+add().then(function (value){
+	document.getElementById("output").innerHTML=value;
+});
